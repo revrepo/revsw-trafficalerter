09f718406a0f4f085d630d73f09bcd6d2f066689
AsherMoshav <asher@mediadome.co.il>
2018-06-16 19:06:27 +0300

    Changed TrafficAlertConfig model

--- models/TrafficAlertConfig.js
+++ models/TrafficAlertConfig.js
@@ -23,17 +23,20 @@ const APIMongo = require('./../lib/mongoAPIConn');
 
 let TrafficAlertConfigSchema = new mongoose.Schema({
     account_id: { type: mongoose.SchemaTypes.ObjectId },
+    name: { type: String },
     target_type: { type: String },
     target: { type: String },
     rule_type: { type: String },
     rule_config: { type: Object },
-    status: { type: String },
+    status: { type: String, default: 'unknown' },
     created_at: { type: Date, default: Date.now() },
     created_by: { type: String },
     updated_at: { type: Date, default: Date.now() },
     updated_by: { type: String },
-    notification_list_id: { type: mongoose.SchemaTypes.ObjectId }
-
+    last_hit: { type: Date, default: null },
+    last_run: { type: Date, default: null },
+    last_dispatch: { type: Date, default: null },
+    notifications_list_id: { type: mongoose.SchemaTypes.ObjectId }
 });
 
 const TrafficAlertConfig = APIMongo.model('TrafficAlertConfig', TrafficAlertConfigSchema, 'TrafficAlertConfig');
